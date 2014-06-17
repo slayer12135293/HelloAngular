aeb6a38bb22e03b444d4dd826360114ff4c8d765
lei.liu@apollo.se <lei.liu@apollo.se>
2014-06-17 13:51:24 +0200

    updaaaaaaaaaaaaaaaaaaaaate

--- angulardemo/Scripts/angular/Controllers.js
+++ angulardemo/Scripts/angular/Controllers.js
@@ -15,15 +15,14 @@ LeiDemoApp.controller('Link1controller', ['$scope', function ($scope) {
         }
     };
 
-    $scope.bigBanner = { header: 'Welcome to Link1', text:'Lots of amazing things will happen here' }
-
+    $scope.bigBanner = { header: 'Welcome to Link1', text: 'Lots of amazing things will happen here' };
 
 
 }]);
 
 LeiDemoApp.controller('Link2controller', ['$scope', '$location', function ($scope, $location) {
 
-    $scope.bigBanner = { header: 'Welcome to Link2', text: 'just as amazing as link1' }
+    $scope.bigBanner = { header: 'Welcome to Link2', text: 'just as amazing as link1' };
   
     $scope.popover1 = {
         imageurl: 'http://lakernation.com/forums/public/style_emoticons/default/homer.gif',
